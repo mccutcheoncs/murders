51b7c07de6fe0e8c81b474b1f0e6e286c43d26e2
= <=>
2018-11-07 08:48:32 -0600

    Adding all

---
+++ download-data.R
@@ -0,0 +1,8 @@
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dest_file <- "data/murders.csv"
+download.file(url, destfile = dest_file)
+
+
+url <- "https://raw.githubusercontent.com/rairizarry/murders/master/report.Rmd"
+dest_file <- "report.Rmd"
+download.file(url, destfile = dest_file)
